0b620e7360f5f8ce07b92c569d5d23870bf0939b
Nagaraz C <raajdevops2026@gmail.com>
2026-02-12 20:50:10 +0530

    inf

--- 06-variables.sh
+++ 06-variables.sh
@@ -4,9 +4,9 @@ START_TIME=$(date +%s)
 
 echo "Script executed at: $START_TIME"
 
-sleep 10
+#sleep 10
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(($END_TIME-$START_TIME))
+#TOTAL_TIME=$(($END_TIME-$START_TIME))
 
 echo "Script executed in: $TOTAL_TIME seconds"
