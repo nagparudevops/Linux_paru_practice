cc0df85de9c64cb3121db47f24d89878ed1655a6
Nagaraz C <raajdevops2026@gmail.com>
2026-05-05 19:28:28 +0530

    jenkins

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -3,5 +3,7 @@
 NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
- echo "Given number is : $NUMBER is greater than 20"
+   echo "Given number is : $NUMBER is greater than 20"
  else
+        echo "Given number is : $NUMBER is less than 20"
+fi
